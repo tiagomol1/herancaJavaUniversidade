fdd1cfc9f69e6c72eaa85d13dc928274622c22a8
Tiago Murilo <62064267+tiagomol1@users.noreply.github.com>
2020-11-04 22:41:03 -0300

    ...

--- exercicio4/Square.java
+++ exercicio4/Square.java
@@ -1,7 +1,22 @@
 public class Square extends Rectangle{
 
-  public Square() {
-    
+  private Double side;
+
+  public Square(String color, boolean filled, Double side) {
+    super(color, filled, side, side);
+    this.side = side;
+  }
+
+  public Double getSide(){
+    return this.side;
+  }
+
+  public void setSide(Double side){
+    this.side = side;
+  }
+
+  public String toString(){
+    return super.toString();
   }
 
 }
--- exercicio4/Test.java
+++ exercicio4/Test.java
@@ -5,22 +5,20 @@ public class Test {
     Circle circle = new Circle("red", true, 3.2);
     System.out.println("Circle: ");
     System.out.println(circle);
-    System.out.println(circle.getPerimeter());
-    System.out.println(circle.getArea());
+    System.out.println("Perimetro: " + circle.getPerimeter());
+    System.out.println("Area: " + circle.getArea());
     System.out.println("-------------------------------------");
     
     Rectangle rectangle = new Rectangle("blue", false, 10.2, 21.4);
     System.out.println("Rectangle: ");
     System.out.println(rectangle);
-    System.out.println(rectangle.getPerimeter());
-    System.out.println(rectangle.getArea());
+    System.out.println("Perimetro: " + rectangle.getPerimeter());
+    System.out.println("Area: " + rectangle.getArea());
     System.out.println("-------------------------------------");
     
-    Rectangle rectangle = new Rectangle("blue", false, 10.2, 21.4);
-    System.out.println("Rectangle: ");
-    System.out.println(rectangle);
-    System.out.println(rectangle.getPerimeter());
-    System.out.println(rectangle.getArea());
+    Square square = new Square("green", true, 10.2);
+    System.out.println("Square: ");
+    System.out.println(square);
     System.out.println("-------------------------------------");
   }
 
